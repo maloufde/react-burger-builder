2b2c11764f26541ffe7394ae0b31f49b973b337c
Christian Malouf <chris@malouf.de>
2019-04-10 06:00:50 +0200

    render ingredient based on type

--- src/components/Burger/Burgeringredient/Burgeringredient.js
+++ src/components/Burger/Burgeringredient/Burgeringredient.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import styles from './BurgerIngredient.module.css';
 
 const burgerIngredient = (props) => {
-    return <div>ingredient</div>
+    let ingredient = null;
+
+    switch(props.type){
+        case ('bread-top'): {
+            ingredient = (
+                <div className={styles.BreadTop}>
+                    <div className={styles.Seeds1}></div>
+                    <div className={styles.Seeds2}></div>
+                </div>
+            );
+            break;
+        }
+        case ('cheese'): {
+            ingredient = <div className={styles.Cheese}></div>
+            break;
+        }
+        case ('salad'): {
+            ingredient = <div className={styles.Salad}></div>
+            break;
+        }
+        case ('bacon'): {
+            ingredient = <div className={styles.Bacon}></div>
+            break;
+        }
+        case ('meat'): {
+            ingredient = <div className={styles.Meat}></div>
+            break;
+        }
+        case ('bread-bottom'): {
+            ingredient = <div className={styles.BreadBottom}></div>
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+
+    return ingredient;
 };
 
 export default burgerIngredient;
