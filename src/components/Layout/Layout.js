20e8536b91b891e57bffb0a1a420d51c72e9d64a
Christian Malouf <chris@malouf.de>
2019-04-10 05:24:23 +0200

    add some style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Layout.module.css';
 
 const layout = (props) => (
   <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={styles.Content}>
       {props.children}
     </main>
   </React.Fragment>
