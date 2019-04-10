c7b9a4611a2742906648b9d23b0b005146bd1949
Christian Malouf <chris@malouf.de>
2019-04-10 05:38:14 +0200

    add Burger with Ingredients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BurgerIngredient from './Burgeringredient/Burgeringredient';
+
+const burger = (props) => (
+  <div>
+      <BurgerIngredient/>
+      <BurgerIngredient/>
+      <BurgerIngredient/>
+  </div>
+);
+
+export default burger;
---
+++ src/components/Burger/Burgeringredient/Burgeringredient.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const burgerIngredient = (props) => {
+    return <div>ingredient</div>
+};
+
+export default burgerIngredient;
