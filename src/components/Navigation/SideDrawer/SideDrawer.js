cb042dbbec93ecf4c71a7a385ce1fab6aa6c6749
Christian Malouf <chris@malouf.de>
2019-04-13 18:17:41 +0200

    Also use backdrop when sidebar shown

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,21 @@ import React from 'react';
 import styles from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
   return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo/>
+    <React.Fragment>
+      <Backdrop show/>
+      <div className={styles.SideDrawer}>
+        <div className={styles.Logo}>
+         <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+    </React.Fragment>
   );
 };
 
